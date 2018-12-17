0575993136780569bc2cec11668a4087bafc330a
cardehngeh <cbkadenge@gmail.com>
2018-12-18 02:30:00 +0300

    adding a css loader

---
+++ assets/styles/javacript.js
@@ -0,0 +1,9 @@
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".wrap").length > 0)
+        {
+            $(".wrap").fadeOut("slow");
+        }
+    });
+})(jQuery)
